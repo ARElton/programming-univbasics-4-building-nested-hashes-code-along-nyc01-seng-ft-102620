927c425059b696122880148aa39bd44702b75ddd
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-05 18:55:31 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,10 +6,12 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {
-        :name =>
-        :age =>
-        :attitude => 
+        :name => "Benvolio",
+        :age => "17",
+        :attitude => "worried"
       },
+      {
+      }
       ]
    }, 
    :capulet => {
